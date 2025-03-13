c908e22957c47daa49d847a7ac435ba54d65a00e
maneetHTML <maneet.harmeet@gmail.com>
2025-03-13 16:37:48 +1300

    after

---
+++ after.py
@@ -0,0 +1,9 @@
+
+import turtle
+mywindow=turtle.Screen()
+mywindow.bgcolor("lightgreen")
+mypen=turtle.Turtle()
+for i in range(4):
+    mypen.fd(100)
+    mypen.left(90)
+turtle.done()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+
+mypen=turtle.Turtle()
